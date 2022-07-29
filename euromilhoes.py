4dfdee52c54296b5cf6137585ad23e53ebafb9c4
joaopinto15 <pintojad03@gmail.com>
2022-07-29 08:31:21 +0100

    Gui update

--- euromilhoes.py
+++ euromilhoes.py
@@ -18,23 +18,27 @@ def getStar():
 def printCode():
     
     label = Label(root,text="Stars: "+str(getNumbers()) +"Numbers: "+ str(getStar()))
-    label.pack()
+    label.place(x=150,y=150)
 
 
 root = Tk()
+root.config(background="black")
 #Screen Resolution
 width = root.winfo_screenwidth()
 height = root.winfo_screenheight()
 #Program position
-x = width/2 - 300
-y = height/2 - 300
+x = width/2 -  500
+y = height/2 - 200
 #Create a title
 root.title("Gerador Euromilhões")
-root.geometry("%dx%d+%d+%d"%(300,300,x,y))
+root.geometry("%dx%d+%d+%d"%(500,200,x,y))
 root.resizable(False,False)
+#Create a label
+label = Label(root,text="Euromilhões number Generator",font=('Arial',20,'bold'),fg='#00FF00',bg='black')
+label.place(x=30,y=30)
 #Create a button
 
 button = Button(root,text="Gerar",command= lambda: printCode())
-button.pack()
+button.place(x=230,y=100)
 #Loop the program 
 root.mainloop()
